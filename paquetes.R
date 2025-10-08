852ed2c438f1c7dcdc27fb62ea78b995bd559d11
YaelHernG <yaelhgs@gmail.com>
2025-10-08 16:23:28 -0600

    version oct/8/2025

---
+++ paquetes.R
@@ -0,0 +1,17 @@
+# Paquetes necesarios:
+install.packages("rmarkdown")
+# pagedown
+remotes::install_github('rstudio/pagedown', force = TRUE)
+# devtools
+install.packages("devtools")
+# datadriven package
+devtools::install_github("nstrayer/datadrivencv", force = TRUE)
+
+# https://github.com/nstrayer/datadrivencv
+
+# Informacion de como realizarlo de acuerdo con Monica Padilla
+# https://github.com/mpadilla905/mpadilla905.github.io/tree/master
+
+# Curso CV
+# https://github.com/mpadilla905/curso-cv/tree/main/slides
+
